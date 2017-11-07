3e49df00bd8b524b3dafc4a8d7d4079e007fb244
wbjxxzx <wbjxxzx@163.com>
2017-11-07 08:09:35 +0800

    modify view

--- www/handlers.py
+++ www/handlers.py
@@ -3,10 +3,29 @@
 __author__ = 'Ming'
 
 from asyncweb import get
-from models import User
+from models import User, Blog
+import time
+
 @get('/')
 def index(request):
-    users = yield from User.findAll()
+    summary = 'something interesting'
+    blogs = [
+        Blog(id='1', name='test blog', summary=summary, created=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created=time.time()-3600),
+        Blog(id='3', name='learn python', summar=summary, created=time.time()-7200),
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs,
+    }
+
+@get('/login')
+def login(request):
+    return {'__template__': 'login.html'}
+
+@get('/test')
+async def test(request):
+    users = await User.findAll()
     return {
         '__template__': 'test.html',
         'users': users
